1d435bd5ad30b2322bb5a5c864f26229728f62dc
Bartlomiej Rutkowski <b.rutkowski94@gmail.com>
2021-10-17 17:14:04 +0200

    Create s3 bucket and user to upload data

---
+++ aws/outputs.tf
@@ -0,0 +1,12 @@
+output "bucket_name" {
+  value = aws_s3_bucket.code_bucket.bucket
+}
+
+output "user_access_key_id" {
+  value = aws_iam_access_key.user_key.id
+}
+
+output "user_secret_access_key" {
+  value = aws_iam_access_key.user_key.secret
+  sensitive = true
+}
---
+++ aws/user.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_user" "github_user" {
+  name = "github_upload_code_user"
+}
+
+resource "aws_iam_user_policy" "user_policy" {
+  name_prefix = "github_upload_code_user"
+  user = aws_iam_user.github_user.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:Get*",
+                "s3:List*",
+                "s3:PutObject"
+            ],
+            "Resource": "${aws_s3_bucket.code_bucket.arn}"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_access_key" "user_key" {
+  user = aws_iam_user.github_user.name
+}
