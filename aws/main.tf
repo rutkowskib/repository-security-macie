7502d5dfbc85a4328f0bf177761ebecc68668fc2
Bartlomiej Rutkowski <b.rutkowski94@gmail.com>
2021-10-18 14:58:23 +0200

    Add sns integration

--- aws/main.tf
+++ aws/main.tf
@@ -5,23 +5,13 @@ resource "aws_s3_bucket" "code_bucket" {
 
 data "aws_iam_account_alias" "account" {}
 
-resource "aws_macie2_account" "macie_account" {}
+resource "aws_macie2_account" "macie_account" {
+  finding_publishing_frequency = "FIFTEEN_MINUTES"
+}
 
 resource "aws_macie2_custom_data_identifier" "stripe_identifier" {
   name                   = "Stripe secret"
   regex                  = "STRIPE_SECRET"
 
-  depends_on = [ aws_macie2_account.macie_account ]
-}
-
-resource "aws_macie2_classification_job" "classification" {
-  job_type = "ONE_TIME"
-  name     = "repo_security"
-  s3_job_definition {
-    bucket_definitions {
-      account_id = aws_macie2_account.macie_account.id
-      buckets    = [ aws_s3_bucket.code_bucket.bucket ]
-    }
-  }
   depends_on = [ aws_macie2_account.macie_account ]
 }
---
+++ aws/sns.tf
@@ -0,0 +1,44 @@
+resource "aws_cloudwatch_event_rule" "macie_finding" {
+  name        = "macie_finding"
+
+  event_pattern = <<EOF
+{
+  "source": ["aws.macie"],
+  "detail-type": ["Macie Finding"]
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_target" "sns" {
+  rule      = aws_cloudwatch_event_rule.macie_finding.name
+  arn       = aws_sns_topic.macie_topic.arn
+}
+
+resource "aws_sns_topic" "macie_topic" {
+  name = "macie_findings"
+}
+
+resource "aws_sns_topic_policy" "policy" {
+  arn    = aws_sns_topic.macie_topic.arn
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  statement {
+    effect  = "Allow"
+    actions = [ "SNS:Publish" ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    resources = [ aws_sns_topic.macie_topic.arn ]
+  }
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.macie_topic.arn
+  protocol  = "email"
+  endpoint  = var.email
+}
